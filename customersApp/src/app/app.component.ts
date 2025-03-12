125bee2fa390150d0a4ef2411a2f54424fc184c1
sofiamoniz <asmedcasmonf@gmail.com>
2025-03-12 14:58:46 +0000

    wip: Simple table

--- customersApp/src/app/app.component.ts
+++ customersApp/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Table } from './Table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [Table],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'customersApp';
-}
+export class AppComponent { }
