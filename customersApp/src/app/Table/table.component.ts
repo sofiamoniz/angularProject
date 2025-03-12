904a52f2eb2379f969002246e384915f7c8d0a64
sofiamoniz <asmedcasmonf@gmail.com>
2025-03-12 16:01:46 +0000

    wip(table): Table working with search filter and pagination

--- customersApp/src/app/Table/table.component.ts
+++ customersApp/src/app/Table/table.component.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 /**
  * @title Table with expandable rows
@@ -10,14 +14,28 @@ import {MatTableModule} from '@angular/material/table';
   selector: 'app-table',
   styleUrl: 'table.component.scss',
   templateUrl: 'table.component.html',
-  imports: [MatTableModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
   standalone: true,
 })
-export class Table {
-  dataSource = ELEMENT_DATA;
+export class Table implements AfterViewInit {
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement!: PeriodicElement | null;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
 
   /** Checks whether an element is expanded. */
   isExpanded(element: PeriodicElement) {
@@ -27,6 +45,20 @@ export class Table {
   /** Toggles the expanded state of an element. */
   toggle(element: PeriodicElement) {
     this.expandedElement = this.isExpanded(element) ? null : element;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
 
