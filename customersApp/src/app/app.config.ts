28e3bada1cc6d747e2975fa3edaf9b0a3fefe476
sofiamoniz <asmedcasmonf@gmail.com>
2025-03-14 15:41:42 +0000

    fix(ngrx): Removed useless import

--- customersApp/src/app/app.config.ts
+++ customersApp/src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideState, provideStore } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { userReducer } from './ngrx/user/user.reducer';
 import { provideEffects } from '@ngrx/effects';
 import { UserEffects } from './ngrx/user/user.effects';
