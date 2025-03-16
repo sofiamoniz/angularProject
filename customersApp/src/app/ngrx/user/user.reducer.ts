108689d98ae240c4a7593178c0252698e54b2019
sofiamoniz <asmedcasmonf@gmail.com>
2025-03-16 20:15:59 +0000

    fix(error-vars): Added missing reducer error vars

--- customersApp/src/app/ngrx/user/user.reducer.ts
+++ customersApp/src/app/ngrx/user/user.reducer.ts
@@ -6,14 +6,15 @@ export interface UserState {
   users: User[];
   loading: boolean;
   error: {
-    deleteUser: boolean;
+    deleteUser?: boolean;
+    getUsers?: boolean;
   };
 }
 
 export const initialState: UserState = {
   users: [],
   loading: false,
-  error: { deleteUser: false },
+  error: { deleteUser: false, getUsers: false },
 };
 
 export const userReducer = createReducer(
@@ -21,19 +22,28 @@ export const userReducer = createReducer(
   on(UserActions.loadUsers, (state) => ({
     ...state,
     loading: true,
+    error: { ...state.error, getUsers: undefined },
   })),
   on(UserActions.loadUsersSuccess, (state, { data }) => ({
     ...state,
     users: data,
     loading: false,
+    error: { ...state.error, getUsers: false },
   })),
   on(UserActions.loadUsersFailure, (state) => ({
     ...state,
     loading: false,
+    error: { ...state.error, getUser: true },
+  })),
+  on(UserActions.deleteUser, (state) => ({
+    ...state,
+    loading: true,
+    error: { ...state.error, deleteUser: undefined },
   })),
   on(UserActions.deleteUserSuccess, (state, { id }) => ({
     ...state,
     users: state.users.filter(user => user.id !== id),
+    error: { ...state.error, deleteUser: false },
   })),
   on(UserActions.deleteUserFailure, (state) => ({
     ...state,
